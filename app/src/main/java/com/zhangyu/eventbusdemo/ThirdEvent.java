dabf407670b7b972c3cd741089bc5fcbc6251c97
912348141@qq.com <314159zhang>
2017-02-06 17:12:43 +0800

    commit by zhangyu

---
+++ app/src/main/java/com/zhangyu/eventbusdemo/ThirdEvent.java
@@ -0,0 +1,17 @@
+package com.zhangyu.eventbusdemo;
+
+/**
+ * Created by Administrator on 2017/2/6.
+ */
+
+public class ThirdEvent {
+    private String mMsg;
+    public ThirdEvent(String msg) {
+        // TODO Auto-generated constructor stub
+        mMsg = msg;
+    }
+    public String getMsg(){
+        return mMsg;
+    }
+
+}
